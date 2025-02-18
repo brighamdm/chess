11b8d5017823543e06edff03a7deedbc71ea004f
morgbrig <morgbrig@oregonstate.edu>
2025-02-17 20:01:53 -0700

    wrote AuthDAO interface

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
+import model.AuthData;
+
+import java.util.ArrayList;
+
 public interface AuthDAO {
+
+    ArrayList<AuthData> auths = new ArrayList<AuthData>();
+
+    static void clear() {
+        auths.clear();
+    }
+
+    static void createAuth(AuthData newAuth) {
+        auths.add(newAuth);
+    }
+
+    static AuthData getAuth(String authToken) {
+        AuthData found = null;
+        for (AuthData a : auths) {
+            if (a.authToken().equals(authToken)) {
+                found = a;
+                break;
+            }
+        }
+        return found;
+    }
 }
