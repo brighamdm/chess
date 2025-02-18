a4573be30baafd73649fb1c377990a927b89ca7d
morgbrig <morgbrig@oregonstate.edu>
2025-02-17 21:57:55 -0700

    finished Game Service Class

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,39 +1,76 @@
 package service;
 
 import chess.ChessGame;
-import model.AuthData;
-import model.CreateRequest;
-import model.CreateResult;
-import model.GameData;
+import model.*;
 
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static dataaccess.AuthDAO.getAuth;
-import static dataaccess.GameDAO.createGame;
-import static dataaccess.GameDAO.getGame;
+import static dataaccess.GameDAO.*;
 
 public class GameService implements Service {
 
     public CreateResult create(CreateRequest createRequest) {
-        AuthData auth = getAuth(createRequest.authToken());
-        if (auth != null) {
-            int gameID = ThreadLocalRandom.current().nextInt(1000, 10000);
-            GameData game = getGame(gameID);
-            if (game == null) {
-                game = new GameData(gameID,
-                        null, null,
-                        createRequest.gameName(),
-                        new ChessGame());
-                createGame(game);
+        if (authExists(createRequest.authToken())) {
+            int gameID;
+            while (true) {
+                gameID = ThreadLocalRandom.current().nextInt(1000, 10000);
+                GameData game = getGame(gameID);
+                if (game == null) {
+                    game = new GameData(gameID,
+                            null, null,
+                            createRequest.gameName(),
+                            new ChessGame());
+                    createGame(game);
 
-                return new CreateResult(null, gameID);
-            } else {
-                return new CreateResult("Error: ", -1);
+                    break;
+                }
             }
+            return new CreateResult(null, gameID);
         } else {
-            return new CreateResult("Error: ", -1);
+            return new CreateResult("Error: Invalid AuthToken", -1);
         }
     }
 
+    public JoinResult join(JoinRequest joinRequest) {
+        AuthData auth = getAuth(joinRequest.authToken());
+        if (auth != null) {
+            GameData game = getGame(joinRequest.gameID());
+            if (game != null) {
+                if (joinRequest.playerColor().equals("WHITE")) {
+                    updateGame(new GameData(game.gameID(),
+                            auth.username(),
+                            game.blackUsername(),
+                            game.gameName(),
+                            game.game()));
 
+                    return new JoinResult(null);
+                } else if (joinRequest.playerColor().equals("BLACK")) {
+                    updateGame(new GameData(game.gameID(),
+                            game.whiteUsername(),
+                            auth.username(),
+                            game.gameName(),
+                            game.game()));
+
+                    return new JoinResult(null);
+                } else {
+                    return new JoinResult("Error: Invalid Player Color");
+                }
+            } else {
+                return new JoinResult("Error: Game does not exist");
+            }
+        } else {
+            return new JoinResult("Error: Invalid AuthToken");
+        }
+    }
+
+    public ListResult list(LogoutRequest listRequest) {
+        if (authExists(listRequest.authToken())) {
+            return new ListResult(null,
+                    (List<GameData>) listGames());
+        } else {
+            return new ListResult("Error: Invalid AuthToken", null);
+        }
+    }
 }
--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -1,10 +1,19 @@
 package service;
 
+import model.AuthData;
+
 import java.util.UUID;
+
+import static dataaccess.AuthDAO.getAuth;
 
 public interface Service {
 
     default String generateToken() {
         return UUID.randomUUID().toString();
+    }
+
+    default boolean authExists(String authToken) {
+        AuthData auth = getAuth(createRequest.authToken());
+        return auth != null;
     }
 }
