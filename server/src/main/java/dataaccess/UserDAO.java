bddd8728db88662f6c182c4fd1122e676d2f1269
morgbrig <morgbrig@oregonstate.edu>
2025-02-17 19:47:51 -0700

    wrote UserDAO interface

---
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -0,0 +1,29 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.util.ArrayList;
+
+public interface UserDAO {
+
+    ArrayList<UserData> users = new ArrayList<UserData>();
+
+    static void clear() {
+        users.clear();
+    }
+
+    static void createUser(UserData newUser) {
+        users.add(newUser);
+    }
+
+    static UserData getUser(String username) {
+        UserData found = null;
+        for (UserData u : users) {
+            if (u.username().equals(username)) {
+                found = u;
+                break;
+            }
+        }
+        return found;
+    }
+}
