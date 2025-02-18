05e002219ae1a4b7efd8fa563c223d282615e67e
morgbrig <morgbrig@oregonstate.edu>
2025-02-17 19:55:42 -0700

    wrote GameDAO interface

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,43 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public interface GameDAO {
+
+    ArrayList<GameData> games = new ArrayList<GameData>();
+
+    static void clear() {
+        games.clear();
+    }
+
+    static void createGame(GameData newGame) {
+        games.add(newGame);
+    }
+
+    static GameData getGame(int gameID) {
+        GameData found = null;
+        for (GameData u : games) {
+            if (u.gameID() == gameID) {
+                found = u;
+                break;
+            }
+        }
+        return found;
+    }
+
+    static Collection<GameData> listGames() {
+        return games;
+    }
+
+    static void updateGame(GameData updatedGame) {
+        for (int i = 0; i < games.size(); i++) {
+            if (games.get(i).gameID() == updatedGame.gameID()) {
+                games.set(i, updatedGame);
+                break;
+            }
+        }
+    }
 }
