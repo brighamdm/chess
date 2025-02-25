9707158c5937db4ae01de1e1c7e7f4149e5f4731
morgbrig <morgbrig@oregonstate.edu>
2025-02-25 12:08:57 -0700

    Some API tests passing

--- server/src/main/java/handler/ClearHandler.java
+++ server/src/main/java/handler/ClearHandler.java
@@ -6,7 +6,7 @@ import spark.*;
 
 public class ClearHandler implements Handler {
 
-    public Object clearHandler(Request req, Response res) { //noinspection UnusedParameter
+    public Object clearHandler(Request unusedReq, Response res) { //noinspection UnusedParameter
 
         ClearService service = new ClearService();
         ClearResult result = service.clear();
