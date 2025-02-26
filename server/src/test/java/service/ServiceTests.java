98d67cb81c93bcb74f0d2f746d57e275f3e5aa0e
morgbrig <morgbrig@oregonstate.edu>
2025-02-25 20:34:52 -0700

    fixed service tests

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -22,7 +22,7 @@ public class ServiceTests {
     }
 
     @Test
-    public void clearTest() throws UnavailableException, BadRequestException {
+    public void clearSuccess() throws UnavailableException, BadRequestException {
         // Positive
         userService.register(new RegisterRequest("bm888",
                 "brickwall", "bm888@byu.edu"));
@@ -32,15 +32,21 @@ public class ServiceTests {
     }
 
     @Test
-    public void register() throws UnavailableException, BadRequestException {
+    public void registerSuccess() throws UnavailableException, BadRequestException {
         // Positive
         RegisterResult result = userService.register(new RegisterRequest("bm888",
                 "brickwall", "bm888@byu.edu"));
         Assertions.assertNotNull(result);
         Assertions.assertNotNull(result.authToken());
         Assertions.assertEquals("bm888", result.username());
+    }
 
+    @Test
+    public void registerFail() throws UnavailableException, BadRequestException {
         // Negative
+        userService.register(new RegisterRequest("bm888",
+                "brickwall", "bm888@byu.edu"));
+
         Assertions.assertThrows(UnavailableException.class, () ->
                 userService.register(new RegisterRequest("bm888",
                         "newpassword", "bm888@byu.edu")));
@@ -51,20 +57,27 @@ public class ServiceTests {
     }
 
     @Test
-    public void logout() throws UnavailableException, BadRequestException, UnauthorizedException {
+    public void logoutSuccess()
+            throws UnavailableException, BadRequestException, UnauthorizedException {
         // Positive
         RegisterResult registerResult = userService.register(new RegisterRequest("bm888",
                 "brickwall", "bm888@byu.edu"));
 
         LogoutResult logoutResult = userService.logout(new LogoutRequest(registerResult.authToken()));
         Assertions.assertNotNull(logoutResult);
+    }
 
+    @Test
+    public void logoutFail() throws UnavailableException, BadRequestException {
         // Negative
+        userService.register(new RegisterRequest("bm888",
+                "brickwall", "bm888@byu.edu"));
+
         Assertions.assertThrows(BadRequestException.class, () -> userService.logout(null));
     }
 
     @Test
-    public void login() throws UnavailableException, BadRequestException, UnauthorizedException {
+    public void loginSuccess() throws UnavailableException, BadRequestException, UnauthorizedException {
         // Positive
         RegisterResult registerResult = userService.register(new RegisterRequest("bm888",
                 "brickwall", "bm888@byu.edu"));
@@ -75,14 +88,20 @@ public class ServiceTests {
         Assertions.assertNotNull(result);
         Assertions.assertNotNull(result.authToken());
         Assertions.assertEquals("bm888", result.username());
+    }
 
+    @Test
+    public void loginFail() throws UnavailableException, BadRequestException {
         // Negative
+        userService.register(new RegisterRequest("bm888",
+                "brickwall", "bm888@byu.edu"));
+
         Assertions.assertThrows(UnauthorizedException.class,
                 () -> userService.login(new LoginRequest("loser", "brickwall")));
     }
 
     @Test
-    public void create() throws UnauthorizedException, BadRequestException, UnavailableException {
+    public void createSuccess() throws UnauthorizedException, BadRequestException, UnavailableException {
         // Positive
         RegisterResult registerResult = userService.register(new RegisterRequest("bm888",
                 "brickwall", "bm888@byu.edu"));
@@ -91,14 +110,17 @@ public class ServiceTests {
                 new CreateRequest("newGame", registerResult.authToken()));
         Assertions.assertNotNull(result);
         Assertions.assertNotEquals(0, result.gameID());
+    }
 
+    @Test
+    public void createFail() {
         // Negative
         Assertions.assertThrows(UnauthorizedException.class,
                 () -> gameService.create(new CreateRequest("newGame", "1234")));
     }
 
     @Test
-    public void list() throws UnavailableException, BadRequestException, UnauthorizedException {
+    public void listSuccess() throws UnavailableException, BadRequestException, UnauthorizedException {
         // Positive
         RegisterResult registerResult = userService.register(new RegisterRequest("bm888",
                 "brickwall", "bm888@byu.edu"));
@@ -111,14 +133,24 @@ public class ServiceTests {
         Assertions.assertNotNull(listResult);
         Assertions.assertNotNull(listResult.games());
         Assertions.assertEquals(3, listResult.games().size());
+    }
 
+    @Test
+    public void listFail() throws UnavailableException, BadRequestException, UnauthorizedException {
         // Negative
+        RegisterResult registerResult = userService.register(new RegisterRequest("bm888",
+                "brickwall", "bm888@byu.edu"));
+
+        gameService.create(new CreateRequest("newGame1", registerResult.authToken()));
+        gameService.create(new CreateRequest("newGame2", registerResult.authToken()));
+        gameService.create(new CreateRequest("newGame3", registerResult.authToken()));
+
         Assertions.assertThrows(UnauthorizedException.class,
                 () -> gameService.list(new ListRequest("1234")));
     }
 
     @Test
-    public void join() throws UnavailableException, BadRequestException, UnauthorizedException {
+    public void joinSuccess() throws UnavailableException, BadRequestException, UnauthorizedException {
         // Positive
         RegisterResult registerResult = userService.register(new RegisterRequest("bm888",
                 "brickwall", "bm888@byu.edu"));
@@ -135,8 +167,20 @@ public class ServiceTests {
         Assertions.assertNotNull(listResult.games());
         Assertions.assertEquals(1, listResult.games().size());
         Assertions.assertEquals("bm888", listResult.games().getFirst().whiteUsername());
+    }
 
+    @Test
+    public void joinFail() throws UnavailableException, BadRequestException, UnauthorizedException {
         // Negative
+        RegisterResult registerResult = userService.register(new RegisterRequest("bm888",
+                "brickwall", "bm888@byu.edu"));
+
+        CreateResult createResult = gameService.create(
+                new CreateRequest("newGame", registerResult.authToken()));
+
+        gameService.join(new JoinRequest("WHITE",
+                createResult.gameID(), registerResult.authToken()));
+
         Assertions.assertThrows(UnavailableException.class,
                 () -> gameService.join(
                         new JoinRequest("WHITE",
