cd0768cbde7101b7bcac85278a9730717b2516fc
morgbrig <morgbrig@oregonstate.edu>
2025-03-17 10:25:44 -0600

    set up client main so it can run with multiple instances, also set up server main

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -2,11 +2,10 @@ import chess.*;
 import server.Server;
 
 public class Main {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
 
+    public static void main(String[] args) {
         Server server = new Server();
-        server.run(8080);
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
     }
 }
